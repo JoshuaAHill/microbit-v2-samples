3bfda01ce869cb67ea63a5f1f7b15c50697c28b4
Joshua Hill <j.hill8@lancaster.ac.uk>
2021-02-23 17:35:59 +0000

    Added updated mic test file, desmonstrating use of mic init in ubit

--- source/samples/MicrophoneTest.cpp
+++ source/samples/MicrophoneTest.cpp
@@ -1,15 +1,20 @@
 #include "MicroBit.h"
 #include "SerialStreamer.h"
-#include "StreamNormalizer.h"
 #include "LevelDetector.h"
 #include "LevelDetectorSPL.h"
+#include "StreamNormalizer.h"
+#include "StreamSplitter.h"
+#include "MicroBitAudioProcessor.h"
 #include "Tests.h"
+
 
 static NRF52ADCChannel *mic = NULL;
 static SerialStreamer *streamer = NULL;
 static StreamNormalizer *processor = NULL;
 static LevelDetector *level = NULL;
 static LevelDetectorSPL *levelSPL = NULL;
+static StreamSplitter *splitter = NULL;
+static MicroBitAudioProcessor *fft = NULL;
 static int claps = 0;
 static volatile int sample;
 
@@ -82,17 +87,51 @@ mems_clap_test(int wait_for_clap)
     if (processor == NULL)
         processor = new StreamNormalizer(mic->output, 1.0f, true, DATASTREAM_FORMAT_UNKNOWN, 10);
 
-    if (level == NULL)
-        level = new LevelDetector(processor->output, 150, 75);
+    if (splitter == NULL)
+        splitter = new StreamSplitter(processor->output);
 
-    uBit.io.runmic.setDigitalValue(1);
-    uBit.io.runmic.setHighDrive(true);
+    if (level == NULL)
+        level = new LevelDetector(*splitter, 150, 75);
 
     uBit.messageBus.listen(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_HIGH, onLoud);
     uBit.messageBus.listen(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_LOW, onQuiet);
 
-    while(!wait_for_clap || (wait_for_clap && claps < 3))
+    while(!wait_for_clap || (wait_for_clap && claps < 3)){
         uBit.sleep(1000);
+    }
+
+    uBit.messageBus.ignore(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_HIGH, onLoud);
+    uBit.messageBus.ignore(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_LOW, onQuiet);
+}
+
+void
+mems_clap_test_2(int wait_for_clap)
+{
+    claps = 0;
+
+    // if (mic == NULL){
+    //     mic = uBit.adc.getChannel(uBit.io.microphone);
+    //     mic->setGain(7,0);
+    // }
+
+    // if (processor == NULL)
+    //     processor = new StreamNormalizer(mic->output, 1.0f, true, DATASTREAM_FORMAT_UNKNOWN, 10);
+
+    // if (splitter == NULL)
+    //     splitter = new StreamSplitter(processor->output);
+
+    // if (level == NULL)
+    //     level = new LevelDetector(*splitter, 150, 75);
+
+    // uBit.io.runmic.setDigitalValue(1);
+    // uBit.io.runmic.setHighDrive(true);
+
+    uBit.messageBus.listen(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_HIGH, onLoud);
+    uBit.messageBus.listen(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_LOW, onQuiet);
+
+    while(!wait_for_clap || (wait_for_clap && claps < 3)){
+        uBit.sleep(1000);
+    }
 
     uBit.messageBus.ignore(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_HIGH, onLoud);
     uBit.messageBus.ignore(DEVICE_ID_SYSTEM_LEVEL_DETECTOR, LEVEL_THRESHOLD_LOW, onQuiet);
